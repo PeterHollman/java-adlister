caa2a6c7d5c030b212aa07fae970e483d35167ec
Peter Hollman <peterjhollman@gmail.com>
2019-08-20 15:22:56 -0500

    added dynamic input box

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,21 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet("/helloworld")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws IOException {
+        System.out.println("Hello World");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String input = req.getParameter("input");
+        out.println("<h1>Hello, " + input + "!</h1>");
+        out.println("<form method = 'get'> <input type = 'text' name = 'input'> </input> <input type = 'submit'> </input> </form>");
     }
 }

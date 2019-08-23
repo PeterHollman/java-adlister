d1ceadbc3cbf4b737ffb1bf9b2594f82701d3087
Peter Hollman <peterjhollman@gmail.com>
2019-08-23 16:29:48 -0500

    finished sessions and cookies project

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,43 +1,30 @@
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        if (request.getSession().getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-//        boolean validAttempt = username.equals("admin") && password.equals("password");
-//
-//        if (validAttempt) {
-//            response.sendRedirect("/profile");
-//        } else {
-//            response.sendRedirect("/login");
-//        }
-        if (username.length() > 0 && password.length() > 0) {
-            HttpSession oldSession = request.getSession(false);
-            if (oldSession != null) {
-                oldSession.invalidate();
-            }
-            HttpSession newSession = request.getSession(true);
+        boolean validAttempt = username.equals("admin") && password.equals("password");
 
-            newSession.setMaxInactiveInterval(5*60);
-
-            Cookie message = new Cookie("message", "Welcome");
-            response.addCookie(message);
+        if (validAttempt) {
+            request.getSession().setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login");
-            PrintWriter out = response.getWriter();
-            out.println("<font color=red>Either username or password is wrong.</font>");
-            rd.include(request, response);
+            response.sendRedirect("/login");
         }
     }
 }
